a6f0f5be6c862fef20537bc3a9b689d38b3da5cd
MStumpo <stumpo.miguel@gmail.com>
2025-08-14 00:04:07 +0200

    Fully fixed and implemented new matrixEntropy

--- adjMatrix.cpp
+++ adjMatrix.cpp
@@ -11,8 +11,14 @@ class AdjacencyMatrix{
 	private:
 		vector<vector<double>> data;
 
-		vector<vector<double>> crossKernelMatrixEntropy2(int kernelSize = 2, bool normalization = false){
-			//NOTE: For some goddamn reason this and the original are giving out different values
+		vector<vector<double>> crossKernelMatrixEntropy(int kernelSize = 2, bool normalization = false){
+			
+			// H = - sum(p*log(p))
+		    // H_norm = H*e/(n*log(e)) where n is total analytes (2*kernel_size +2)
+		    //normalization is of the probabilities of adjMatrix [-1 , 1] -> [0,1], not H_norm
+		    //since our ps are [-1,1] we can do either (p+1)/2 normalization or sum(|p|*log(|p|))
+		    //these two strats can be switched around via the normalization bool
+
 		    vector<vector<double>> entropy(data.size(), vector<double>(data[0].size(), 0.0));
 			double *row_buffer[kernelSize*2 +1];
 			int a = 0;
@@ -26,6 +32,8 @@ class AdjacencyMatrix{
 
 				for(int j = 0; j < data[i].size(); j++){
 
+					int n = 0;
+
 					//condition to prevent out of bounds attributing: stops at edge of matrix
 					if(j+kernelSize < data[i].size()){
 						row_buffer[a] = &data[i][j+kernelSize];
@@ -34,20 +42,30 @@ class AdjacencyMatrix{
 					}
 					a = (a+1)%(kernelSize*2 + 1);
 
-					//This is to move the columns because their pointers are completely new in a j shift
-					for(int k = max(i-kernelSize, 0); k < min(i + kernelSize, (int)data.size()); k++){
-		                if(normalization && data[k][j] != -1 && k != i){
-		                    entropy[i][j] += -((data[k][j]+1)/2)*log((data[k][j]+1)/2);
-		                }else if(!normalization && data[k][j] != 0 && k != i) {
-		                    entropy[i][j] += -abs(data[k][j])*log(abs(data[k][j]));
-		                }
+
+					//Fetching from the cols since they're always new in a colshift
+		            for(int k = 1; i+k < data.size() && k <= kernelSize; k++){
+		            	if(normalization){
+		            		entropy[i][j] += -((data[i+k][j]+1)/2)*log((data[i+k][j]+1)/2);
+		            	}else{
+		            		entropy[i][j] += -abs(data[i+k][j])*log(abs(data[i+k][j]));
+		            	}
+		            	n++;
+		            }
+		           	for(int k = 1; i-k >= 0 && k <= kernelSize; k++){
+		            	if(normalization){
+		            		entropy[i][j] += -((data[i-k][j]+1)/2)*log((data[i-k][j]+1)/2);
+		            	}else{
+		            		entropy[i][j] += -abs(data[i-k][j])*log(abs(data[i-k][j]));
+		            	}
+		            	n++;
 		            }
 
 		            /*Going to the buffer :) note: I'm not doing if the coordinate = ij because I took it off the col loop and I want the actual value  
 		            * to be included in the entropy calculation and it's already in the buffer
 		            */
-
 		            for(auto& buffer : row_buffer){
+
 		            	if(buffer != NULL){
 		            		double val = *buffer;
 			            	if(normalization && val != -1){
@@ -55,54 +73,16 @@ class AdjacencyMatrix{
 			                }else if(!normalization && val != 0) {
 			                    entropy[i][j] += -abs(val)*log(abs(val));
 			                }
+			                n++;
 		            	}
 		            }
-		           	entropy[i][j] *= 2.718281846/(log(2.718281846) * (4*kernelSize+1));
 
-
+		           	entropy[i][j] *= 2.718281846/(log(2.718281846) * n);
 
 				}
 
 			}
 			return entropy;
-		}
-
-		vector<vector<double>> crossKernelMatrixEntropy(int kernelSize = 2, bool normalization = false){
-		    vector<vector<double>> entropy(data.size(), vector<double>(data[0].size(), 0.0));
-
-		    // H = - sum(p*log(p))
-		    // H_norm = H*e/(n*log(e)) where n is total analytes (2*kernel_size +2)
-		    //normalization is of the probabilities of adjMatrix [-1 , 1] -> [0,1], not H_norm
-		    //since our ps are [-1,1] we can do either (p+1)/2 normalization or sum(|p|*log(|p|))
-		    //these two strats can be switched around via the normalization bool
-
-		    for(int i=0; i<data.size(); i++){
-		        for(int j = 0; j < data[i].size(); j++){
-
-		            //0-dim sum -> k substitutes i in matrix
-		            for(int k = max(i-kernelSize, 0); k < min(i+kernelSize, (int)data.size()); k++){
-		                if(normalization && data[k][j] != -1){
-		                    entropy[i][j] += -((data[k][j]+1)/2)*log((data[k][j]+1)/2);
-		                }else if(!normalization && data[k][j] != 0) {
-		                    entropy[i][j] += -abs(data[k][j])*log(abs(data[k][j]));
-
-		                }
-		            }
-
-		            //1-dim sum -> k substitutes j in matrix
-		            for(int k = max(j-kernelSize, 0); k < min(j+kernelSize, (int)data[i].size()); k++){
-		                if(normalization && data[i][k] != -1 && k != j){
-		                    entropy[i][j] += -((data[i][k]+1)/2)*log((data[i][k]+1)/2);
-		                }else if(!normalization && data[i][k] != 0 && k != j){
-		                    entropy[i][j] += -abs(data[i][k])*log(abs(data[i][k]));
-		                }
-		            }
-
-		            entropy[i][j] *= 2.718281846/(log(2.718281846) * (4*kernelSize+1));
-
-		        }
-		    }
-		    return entropy;
 		}
 
 	public:
@@ -131,22 +111,11 @@ class AdjacencyMatrix{
 
         void updateAdj(vector<vector<double>> crossCorrelation, double reward = 1, double lr = 0.001, double reg = 0.001, int kernelSize = 2,  bool kernelNormalization=false, double entropyFactor = 1){
             vector<vector<double>> entropy(data.size(), vector<double>(data[0].size(), 1));
-            vector<vector<double>> entropy2(data.size(), vector<double>(data[0].size(), 1));
 
 
             if (entropyFactor != 0.0){
-                //entropy = crossKernelMatrixEntropy(kernelSize, kernelNormalization);
-				entropy = crossKernelMatrixEntropy2(kernelSize, kernelNormalization);
-                //DEBUG 
-                /*
-                for(int i=0; i < entropy.size(); i++){
-                	for(int j = 0; j < entropy[i].size(); j++){
-                		printf("\nij: %d %d ENTROPY: %f/%f", i, j, entropy[i][j], entropy2[i][j]);
-                	}
-                }
-                printf("\n==================================================================================================================");
-                */
-                //
+				entropy = crossKernelMatrixEntropy(kernelSize, kernelNormalization);
+
             }
             for (int i=0; i < data.size(); i++){
                 for(int j=0; j < data[i].size(); j++){
